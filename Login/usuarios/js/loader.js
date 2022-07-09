75ab75ae7a8d1f02a4d051d9613c2ace6c08d69a
Tomoko559 <108960723+Tomoko559@users.noreply.github.com>
2022-07-08 20:28:23 -0500

    Delete Login/usuarios/js directory

--- Login/usuarios/js/loader.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-	setTimeout(removeLoader, 250);
-     
-});
-function removeLoader(){
-	$(".loader").fadeOut(); //makes page more lightweight  
-	$(".container").fadeIn("fast"); //makes page more lightweight 
-	$(".no_encuentras").fadeIn(); //makes page more lightweight 
-}; 
